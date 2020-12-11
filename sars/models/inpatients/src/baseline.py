76c8f88a3f4ed29d9425d7ff730ca1743d435b4a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-11 22:10:18 +0000

    Add sars/models/inpatients/src/

---
+++ sars/models/inpatients/src/baseline.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+
+class Baseline:
+
+    def __init__(self):
+
+        self.url = 'https://raw.githubusercontent.com/briefings/sars/develop/fundamentals/' \
+                   'atlantic/warehouse/hospitalizations.csv'
+
+        self.fields = ['datetimeobject', 'STUSPS',
+                       'positiveCumulative', 'deathCumulative', 'hospitalizedCumulative',
+                       'positiveRate', 'deathRate', 'hospitalizedRate', 'ndays']
+
+        self.dtype = {'STUSPS': 'str', 'positiveCumulative': np.float64,
+                      'deathCumulative': np.float64, 'hospitalizedCumulative': np.float64,
+                      'positiveRate': np.float64, 'deathRate': np.float64, 'hospitalizedRate': np.float64,
+                      'ndays': np.int64}
+
+        self.parse_dates = ['datetimeobject']
+
+    def exc(self):
+
+        try:
+            return pd.read_csv(filepath_or_buffer=self.url, header=0, usecols=self.fields,
+                               dtype=self.dtype, encoding='utf-8', parse_dates=self.parse_dates)
+        except OSError as err:
+            raise Exception(err.strerror)
