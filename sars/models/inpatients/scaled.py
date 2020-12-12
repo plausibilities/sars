0739c931c64ecbe577fd0d3d35353f3fe27a5a44
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-12-12 16:42:16 +0000

    Update sars/models/inpatients/scaled.py: Updates inline with ...
    ... GitHub Actions error notifications, suggestions; https://github.com/plausibilities/sars/runs/1542511491.

--- sars/models/inpatients/scaled.py
+++ sars/models/inpatients/scaled.py
@@ -1,25 +1,45 @@
+"""
+Module scaled
+"""
+
 import collections
 
 import sars.functions.scaler
 
 
 class Scaled:
+    """
+    Class Scaled
+    """
 
     def __init__(self, data):
         """
 
+        :param data:
         """
+
         self.data = data
 
-        self.T = collections.namedtuple(
-            typename='T',
-            field_names=['xscale', 'independent', 'abscissae', 'yscale', 'dependent'])
+        self.Scaled = collections.namedtuple(
+            typename='Scaled',
+            field_names=['xscale', 'independent', 'abscissae', 'yscale', 'dependent'])  # pylint: disable-msg=C0103
 
     @staticmethod
     def scale():
+        """
+
+        :return:
+        """
+
         return sars.functions.scaler.Scaler()
 
     def independent_(self):
+        """
+        Creates the scale object for independent variables, and the scaled (a) independent tensor,
+        and (b) extended independent tensor that includes prediction points.
+
+        :return:
+        """
 
         scale = self.scale()
         independent = scale.fit_transform(tensor=self.data.independent)
@@ -28,6 +48,11 @@ class Scaled:
         return scale, independent, abscissae
 
     def dependent_(self):
+        """
+        Creates the scale object for dependent variables, and the scaled dependent tensor
+
+        :return:
+        """
 
         scale = self.scale()
         dependent = scale.fit_transform(tensor=self.data.dependent)
@@ -41,4 +66,4 @@ class Scaled:
         """
 
         # noinspection PyUnresolvedReferences,PyProtectedMember
-        return self.T._make(self.independent_() + self.dependent_())
+        return self.Scaled._make(self.independent_() + self.dependent_())
